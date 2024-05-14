c371581eb5d740679ddd107f274984b4ce5a3297
DylanM2023 <145995742+DylanM2023@users.noreply.github.com>
2024-05-14 10:44:57 +0100

    Add files via upload

--- Calc(MAIN).py
+++ Calc(MAIN).py
@@ -1,236 +1,121 @@
 from tkinter import *
 
 calculator = Tk()
-calculator.geometry("470x790")
-calculator.resizable(width=False , height=False)
+calculator.geometry("600x800")
 calculator.title("Calculator App")
-calculator.configure(bg="#222222")
 
-def calculator_functions():
-    def calc_ui():
-        screen = Entry(
-            width=500,
-            font= ("Bold" , 50)
+
+def calc_screen():
+
+    screen_frame = Frame(calculator)
+    screen_frame.configure(
+        width = "600", 
+        height = "100", 
+    )
+    screen_frame.propagate(False)
+    screen_frame.pack(side = TOP , pady = 10)
+
+    display = Entry(
+    screen_frame,
+    font = ("Bold", 50))
+
+    display.bind("<Key>", lambda a: "break") #stops users from entering in entry box
+
+    display.pack(side=RIGHT)
+    
+    def disply_input(number):
+        equation = display.get()
+        if len(equation) >= 1:
+            display.delete("0" , "end")
+            equation += number
+            display.insert(index=False , string = equation)
+        else:
+            display.insert(index = False , string=number)
+
+    def clear_display():
+        display.delete("0" , "end")
+
+    def button_rows():
+
+        
+        button_r_1 = Frame()
+        button_r_1.propagate(False)
+        button_r_1.config(width=580 , height=135 , bg="Red")
+        button_r_1.pack(side = TOP)
+
+        button_r_2 = Frame()
+        button_r_2.propagate(False)
+        button_r_2.config(width=580 , height=135 , bg="white")
+        button_r_2.pack(side =TOP)
+
+        button_r_3 = Frame()
+        button_r_3.propagate(False)
+        button_r_3.config(width=580 , height=135 , bg="black")
+        button_r_3.pack()
+
+        button_r_4 = Frame()
+        button_r_4.propagate(False)
+        button_r_4.config(width=580 , height=135 , bg="yellow")
+        button_r_4.pack()
+
+        button_r_5 = Frame()
+        button_r_5.propagate(False)
+        button_r_5.config(width=580 , height=135 , bg="orange")
+        button_r_5.pack()
+
+        clear_button = Button(
+            button_r_1,
+            text="C",
+            width=10,
+            height=5,
+            command = lambda: clear_display()
         )
-        screen.pack(side=TOP , pady=10)
-        screen.bind("<Key>", lambda a: "break")        
+        one_button = Button(
+            button_r_2,
+            text=" 1 ",
+            width=10,
+            height=5,
+            font= ("Bold" , 15),
+            command = lambda: disply_input("1")
+        )
+        two_button = Button(
+            button_r_2,
+            text=" 2 ",
+            width=10,
+            height=5,
+            font= ("Bold" , 15),
+            command = lambda: disply_input("2")
+
+        )
+        three_button = Button(
+            button_r_2,
+            text=" 3 ",
+            width=10,
+            height=5,
+            font= ("Bold" , 15),
+            command = lambda: disply_input("3")
+        )
+        four_button = Button(
+            button_r_3,
+            text=" 4 ",
+            width=10,
+            height=5,
+            font= ("Bold" , 15),
+            command = lambda: disply_input("4")
+        )
+        clear_button.pack(padx = "5" , pady = "5" , side=LEFT)
+        one_button.pack(padx = "5" , pady = "5" , side=LEFT)
+        two_button.pack(padx = "5" , pady = "5" , side=LEFT)
+        three_button.pack(padx = "5" , pady = "5" , side=LEFT)
+        four_button.pack(padx = "5" , pady = "5" , side=LEFT)
+
+    button_rows()
 
 
-        def display_equation(character):
-            user_input = screen.get()
-            if len(user_input) >= 1:
-                screen.delete("0" , "end")
-                equation = user_input + character
-                screen.insert(index=False , string = equation)
-            else:
-                screen.insert(index=False , string = character)
-
-        def clear_screen():
-            screen.delete("0" , "end")
-
-        def solve():
-            equation = screen.get()
-            screen.delete("0" , "end")
-            print(equation)
-
-        def add_buttons():
-            button_r_1 = Frame(
-                width=550,
-                height=135,
-                bg="Red"
-            )
-
-            button_r_1.propagate(False)
-
-            button_r_2 = Frame(
-                width=550,
-                height=135,
-                bg="Blue"
-            )
-
-            button_r_2.propagate(False)
-
-            button_r_3 = Frame(
-                width=550,
-                height=135,
-                bg="Yellow"
-            )
-
-            button_r_3.propagate(False)
-
-            button_r_4 = Frame(
-                width=550,
-                height=135,
-                bg="Orange"
-            )
-
-            button_r_4.propagate(False)
-
-            button_r_5 = Frame(
-                width=550,
-                height=135,
-                bg="Purple"
-            )
-
-            button_r_5.propagate(False)
-
-            clear_button = Button(
-                button_r_1,
-                width=5,
-                height=2,
-                text="C",
-                font=("Bold",25),
-                command= lambda: clear_screen()
-            )
-
-            one_button = Button(
-                button_r_2,
-                width=5,
-                height=2,
-                text="1",
-                font=("Bold",25),
-                command= lambda: display_equation("1")
-            )
-
-            two_button = Button(
-                button_r_2,
-                width=5,
-                height=2,
-                text="2",
-                font=("Bold",25),
-                command= lambda: display_equation("2")
-            )
-
-            three_button = Button(
-                button_r_2,
-                width=5,
-                height=2,
-                text="3",
-                font=("Bold",25),
-                command= lambda: display_equation("3")
-            )
-
-            multiply_button = Button(
-                button_r_2,
-                width=5,
-                height=2,
-                text="x",
-                font=("Bold",25),
-                command= lambda: display_equation("x")
-            )
-
-            four_button = Button(
-                button_r_3,
-                width=5,
-                height=2,
-                text="4",
-                font=("Bold",25),
-                command= lambda: display_equation("4")
-            )
-
-            five_button = Button(
-                button_r_3,
-                width=5,
-                height=2,
-                text="5",
-                font=("Bold",25),
-                command= lambda: display_equation("5")
-            )
-
-            six_button = Button(
-                button_r_3,
-                width=5,
-                height=2,
-                text="6",
-                font=("Bold",25),
-                command= lambda: display_equation("6")
-            )
-
-            subtract_button = Button(
-                button_r_3,
-                width=5,
-                height=2,
-                text="-",
-                font=("Bold",25),
-                command= lambda: display_equation("-")
-            )
-
-            seven_button = Button(
-                button_r_4,
-                width=5,
-                height=2,
-                text="7",
-                font=("Bold",25),
-                command= lambda: display_equation("7")
-            )
-
-            eight_button = Button(
-                button_r_4,
-                width=5,
-                height=2,
-                text="8",
-                font=("Bold",25),
-                command= lambda: display_equation("8")
-            )
-
-            nine_button = Button(
-                button_r_4,
-                width=5,
-                height=2,
-                text="9",
-                font=("Bold",25),
-                command= lambda: display_equation("9")
-            )
-
-            add_button = Button(
-                button_r_4,
-                width=5,
-                height=2,
-                text="+",
-                font=("Bold",25),
-                command= lambda: display_equation("+")
-            )
-
-            
-            solve_button = Button(
-                button_r_5,
-                width=5,
-                height=2,
-                text="=",
-                font=("Bold",25),
-                command= lambda: solve()
-            )
 
 
-            button_r_1.pack()
-            button_r_2.pack()
-            button_r_3.pack()
-            button_r_4.pack()
-            button_r_5.pack()
+calc_screen()
 
-            clear_button.pack(side=LEFT,padx=10)
-            
-            one_button.pack(side=LEFT , padx=10)
-            two_button.pack(side=LEFT , padx=0)
-            three_button.pack(side=LEFT , padx=10)
-            multiply_button.pack(side=LEFT , padx=0)
-
-            four_button.pack(side=LEFT , padx=10)
-            five_button.pack(side=LEFT , padx=0)
-            six_button.pack(side=LEFT , padx=10)
-            subtract_button.pack(side=LEFT , padx=0)
-
-            seven_button.pack(side=LEFT , padx=10)
-            eight_button.pack(side=LEFT , padx=0)
-            nine_button.pack(side=LEFT , padx=10)
-            add_button.pack(side=LEFT , padx=0)
-
-            solve_button.pack(side=RIGHT , padx=10)
-
-        add_buttons()
-    calc_ui()
-
-calculator_functions()
 
 calculator.mainloop()
 
--- calc(functionality).py
+++ calc(functionality).py
@@ -1,26 +1,41 @@
 equation_multiply = "5x20x2"
 equation_subtract = "100-10"
 equation_subtract_2 = "100-20-5"
+equation_addition = "100+100"
+equation_addition_2 = "100+100+100"
+equation_division = "100÷5"
+equation_division_2 = "100÷5÷2"
 
 def solve(equation):
     for i in equation:
         if i == "x":
-            total = 1
             split_equation = equation.split("x")
-            for i in range(len(split_equation)):
-                total = total * int(split_equation[i])
+            total = int(split_equation[0])
+            for i in range(len(split_equation)-1):
+                total = total * int(split_equation[i+1])
             
         elif i == "-":
-            total = 1
             split_equation = equation.split("-")
-            print(split_equation)
-            for i in range(len(split_equation)):
-                total = total - int(split_equation[i])
+            total = int(split_equation[0])
+            for i in range(len(split_equation)-1):
+                total = total - int(split_equation[i+1])
         elif i == "+":
-            print("not done")
-    print(total)
+            split_equation = equation.split("+")
+            total = int(split_equation[0])
+            for i in range(len(split_equation)-1):
+                total = total + int(split_equation[i+1])
+        elif i == "÷":
+            split_equation = equation.split("÷")
+            total = int(split_equation[0])
+            for i in range(len(split_equation)-1):
+                total = total / int(split_equation[i+1])
+    print(int(total))
 
 
 solve(equation_multiply)
 solve(equation_subtract)
 solve(equation_subtract_2)
+solve(equation_addition)
+solve(equation_addition_2)
+solve(equation_division)
+solve(equation_division_2)
